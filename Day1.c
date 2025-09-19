a94d9a04db305e64b276a333725b9f3ae624f67a
Elmunah lungu <elmunahlungu@gmail.com>
2025-09-19 11:36:18 +0200

    Day 1:Hello C program I/O (printf,fgets,atoi)

--- Day1.c
+++ Day1.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 int main(){
-char name[60];
-int age;
 printf("my name is Elmunah lungu\n");
-printf("my age is 20\n");
+printf("i am 20 years old\n");
 printf("my favorite hobby is cooking\n");
-printf("enter your name:\n");
-scanf("%s",name);
+char name[200];
+int age;
+printf("enter your name:");
+scanf("%s", name);
 printf("enter your age:");
 scanf("%d", &age);
-printf("Hello %s,you are %d years old", name, age);
+printf("hello %s! you are %d years old.\n", name, age);
 return 0;
 }
+
