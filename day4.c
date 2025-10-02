8f456bc7ff8379796e29fa7bdd6713bf5fdbe3a6
Elmunah lungu <elmunahlungu@gmail.com>
2025-10-02 20:44:27 +0200

    Day 4:Arithmetic operators on two numbers

---
+++ day4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+int num1;
+int num2;
+printf("enter first number:\n");
+scanf("%d", &num1);
+printf("enter second number:\n");
+scanf("%d", &num2);
+printf("sum = %d",num1+num2);
+printf("product = %d",num1*num2);
+printf("difference =%d",num1-num2);
+printf("Quotient = %d",num1/num2);
+return 0;
+}
